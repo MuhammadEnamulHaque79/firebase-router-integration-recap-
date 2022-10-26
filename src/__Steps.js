880791a2edf899c74982226538b7368980a6223f
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-26 18:28:38 +0600

    first commit

---
+++ src/__Steps.js
@@ -0,0 +1,11 @@
+/*
+ 1.Create a project into firebase;
+ 2.Register app;
+ 3.npm install firebase;
+ 4.initialize firebase into firebase.init.js; and export
+ default app;
+ 5.Authenticate with sign-in methods;
+ 6.import {BrowserRouter} from 'react-router-dom'and wrap app by it into index.js; 
+ 7.import {Routes,Route} from 'react-router-dom' into App.js;
+ 8.
+ */
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/about'>About</Link>
+
+            </nav>
+        </div>
+
+    );
+};
+
+export default Header;
